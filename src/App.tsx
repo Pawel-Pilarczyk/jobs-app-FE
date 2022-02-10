557fc1f1b8041f3c956cb55ed1116e16c0ef8d44
Pawel Pilarczyk <pawelpilarczyk1@gmail.com>
2022-02-10 08:14:38 +0100

    Project set-up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./style.module.scss";
+
+function App() {
+  return (
+    <div className={styles.test}>
+      <h1>My app</h1>
+    </div>
+  );
+}
+
+export default App;
