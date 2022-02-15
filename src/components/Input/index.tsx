052b62934240891dc63c182161c27bd4ccc70efe
Pawel Pilarczyk <pawelpilarczyk1@gmail.com>
2022-02-15 07:59:44 +0100

    Adding standarize script for css

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,22 +1,40 @@
 import React, { RefObject, useState } from 'react';
-// import cn from 'classnames';
+import cn from 'classnames';
 import { EyeClosed, EyeOpen } from '@assets/svg';
+import styles from './styles.module.scss';
 
 type Props = {
   type: 'text' | 'password';
-  //size: 'large' | 'medium' | 'small';
+  size?: 'small' | 'medium' | 'large';
+  maxLength?: number;
   value: string;
-  onChange: () => void;
+  placeholder?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   ref?: RefObject<HTMLInputElement>;
   error?: string;
 };
 
-const Input = ({ onChange, type, value, error, ref }: Props): JSX.Element => {
+const Input = ({ onChange, type, value, error, ref, maxLength, size = 'medium', placeholder }: Props): JSX.Element => {
   const [showPass, setShowPass] = useState(false);
   const toggleSetShowPassword = () => setShowPass((v) => !v);
+
+  const inputValue = type === 'password' && showPass ? value : value.replace(/[a-z1-9.]/gi, '*');
   return (
-    <div>
-      <input type="text" ref={ref} value={value} onChange={onChange} />
+    <div
+      className={cn(styles.wrapper, {
+        [styles.wrapperSmall]: size === 'small',
+        [styles.wrapperMedium]: size === 'medium',
+        [styles.wrapperLarge]: size === 'large',
+      })}
+    >
+      <input
+        type="text"
+        ref={ref}
+        value={inputValue}
+        onChange={onChange}
+        maxLength={maxLength}
+        placeholder={placeholder}
+      />
       {type === 'password' && showPass ? (
         <EyeOpen onClick={toggleSetShowPassword} />
       ) : (
