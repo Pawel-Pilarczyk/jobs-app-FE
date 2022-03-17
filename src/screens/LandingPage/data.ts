07f96753b13ca5e71816f97c5dc58eb1a4cdd1f3
Pawel Pilarczyk <pawelpilarczyk1@gmail.com>
2022-03-17 22:07:33 +0100

    Building layout

---
+++ src/screens/LandingPage/data.ts
@@ -0,0 +1,6 @@
+export const numericData = [
+  { label: '10,000', description: 'Happy customers' },
+  { label: '259', description: 'Partners' },
+  { label: '16', description: 'Countries' },
+  { label: '6', description: 'Years on the market' },
+];
--- src/screens/LandingPage/index.tsx
+++ src/screens/LandingPage/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Button } from '@components';
+import { Button, Footer } from '@components';
+import { numericData } from './data';
+import image from '@assets/images/landing-page.jpg';
 import styles from './styles.module.scss';
 
 const index = (): JSX.Element => {
@@ -16,8 +18,31 @@ const index = (): JSX.Element => {
           </Button>
         </div>
       </header>
-      <section></section>
-      <footer></footer>
+      <section className={styles.sectionWrapper}>
+        <div className="numericWrapper">
+          {numericData.map(({ label, description }) => (
+            <div key={label}>
+              <h1>{label}</h1>
+              <p>{description}</p>
+            </div>
+          ))}
+        </div>
+        <div className={styles.topSection}>
+          <div>
+            <h1>Headline</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Neque vitae tempus quam pellentesque nec nam.
+            </p>
+            <Button size="large" type="outline" iconRight="arrowRight">
+              Get started
+            </Button>
+          </div>
+          <img src={image} />
+        </div>
+        <div className={styles.bottomSection}></div>
+      </section>
+      <Footer style={styles.footer} />
     </div>
   );
 };
